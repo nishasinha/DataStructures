ffbe9a045ff526bea7f09791f0c43cac530529ce
Nisha <nishasinha68@gmail.com>
2020-09-19 22:58:29 +0530

    Priority Q and DeQ.

---
+++ src/queue/deque.py
@@ -0,0 +1,69 @@
+"""
+Doubly Ended Queue:
+A queue that can support insert and remove from both the ends.
+
+* Operations:
+1. insertRear()
+2. insertFront()
+3. removeFront()
+4. removeRear()
+
+5. getFront()
+6. getRear()
+7. isFull()
+8. isEmpty()
+
+* Implementation:
+1. Doubly linked list
+2. Circular Array
+3. Python collections 'deque' data structure.
+
+They all allow the first 4 operations in O(1) time.
+"""
+from collections import deque
+
+if __name__ == "__main__":
+    print("Double ended Queue!")
+
+    dq = deque([1, 2, 3])
+    print(dq)
+
+    dq.append(4)
+    dq.append(5)
+    print(dq)
+
+    dq.appendleft(0)
+    dq.appendleft(-1)
+    print(dq)
+
+    dq.pop()
+    print(dq)
+    dq.popleft()
+    print(dq)
+
+    dq.insert(2, 10)
+    dq.insert(10, 11)
+    print(dq)
+
+    dq.insert(-1, 12)
+    print(dq)
+
+    dq.remove(1)
+    print(dq)
+
+    dq.reverse()
+    print(dq)
+
+    print(dq.count(12))
+
+    dq.extend([21, 23, 22])
+    print(dq)
+
+    dq.extendleft([-11, -12, -13])
+    print(dq)
+
+    dq.rotate(5)
+    print(dq)
+
+    dq.rotate(-5)
+    print(dq)
---
+++ src/queue/priorityQ.py
@@ -0,0 +1,87 @@
+"""
+Priority Queues are used to operate the FIFO with given priority.
+- Every item has a priority.
+- At dequeue, item with highest priority is removed.
+    If many elements have same priority, FIFo is followed.
+
+* Operations:
+1. insert(item, priority) - O(1)
+2. getHighestPriority() - O(n) in array
+3. deleteHighestPriority() - O(n) in array
+
+* Using heaps:
+Heaps is another data structure that can maintain order of iterables (order from the elements itself).
+Heaps are used to implement priority Q.
+
+1. insert - O(Log n)
+2. delete - O(Log n)
+3. getHighestPriority - O(1)
+
+* Applications:
+1. CPU scheduling of processes
+2. Dijkstra shortest path, Prims MST algo
+
+"""
+import heapq
+
+
+class PriorityQ:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.q = [] * self.capacity
+        heapq.heapify(self.q)
+        self.size = 0
+
+    def _is_empty(self):
+        return self.size == 0
+
+    def _is_full(self):
+        return self.size == self.capacity
+
+    def enqueue(self, item):
+        if self._is_full():
+            print("Overflow!")
+            return
+
+        heapq.heappush(self.q, item)
+        self.size = self.size + 1
+        print("Enqueued {}.".format(item))
+
+    def dequeue(self):
+        if self._is_empty():
+            print("Underflow!")
+            return
+
+        item = heapq.heappop(self.q)
+        self.size = self.size - 1
+        print("Dequeued {}.".format(item))
+
+    def print_q(self):
+        print("Priority Queue is: {}".format(list(self.q)))
+
+
+if __name__ == "__main__":
+    print("Priority Queue")
+
+    pq = PriorityQ(5)
+    pq.print_q()
+
+    print("***** Enqueue *********")
+    pq.enqueue('a')
+    pq.enqueue('z')
+    pq.print_q()
+
+    pq.enqueue('c')
+    pq.enqueue('x')
+    pq.enqueue('d')
+    pq.print_q()
+
+    pq.enqueue('e')
+
+    print("***** Dequeue *********")
+
+    pq.print_q()
+    pq.dequeue()
+    pq.dequeue()
+    pq.dequeue()
+    pq.print_q()
