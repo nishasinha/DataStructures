0d6f6942dadbed9f8bc6c67c8e9f3b619da9edef
Nisha <nishasinha68@gmail.com>
2020-10-07 23:44:44 +0530

    Package structure change.

---
+++ src/design_patterns/intro.py
@@ -0,0 +1,40 @@
+"""
+Design patterns are some industry proven ways of designing problem solutions.
+
+Types:
+1. Creational
+Deals with object and class creation mechanism.
+Eg:
+Factory,
+Abstract Factory,
+Builder,
+Prototype,
+Singleton
+
+
+2. Structural
+Deals with how the larger code structures are formed with smaller structures to provide new features.
+Eg:
+Adapter,
+Bridge,
+Composite,
+Decorator,
+Facade,
+Proxy,
+Flyweight
+
+3. Behavioral
+Deals with communication mechanism between objects.
+Eg:
+Chain of responsibility,
+Command,
+Iterator,
+Mediator,
+Observer,
+State,
+Strategy,
+Template,
+Visitor
+
+
+"""
